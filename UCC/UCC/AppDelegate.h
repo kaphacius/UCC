1a529c52b034b11d2a398f50578f7c241ee12361
Yurii Zadoianchuk <yuriizadoianchuk1@Yuriis-MBP.fritz.box>
2015-02-01 14:15:31 +0100

    adds new project

---
+++ UCC/UCC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UCC
+//
+//  Created by Yurii Zadoianchuk on 01/02/15.
+//  Copyright (c) 2015 Yurii Zadoianchuk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UCC/UCC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UCC
+//
+//  Created by Yurii Zadoianchuk on 01/02/15.
+//  Copyright (c) 2015 Yurii Zadoianchuk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
